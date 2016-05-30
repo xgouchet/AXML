b126ba29be299e7ce5b2ce9107394a0b707f9bb6
Xavier Gouchet <github@xgouchet.fr>
2016-05-30 08:08:19 +0200

    Switch to AS

---
+++ library/src/test/java/fr/xgouchet/axml/AndroidXmlParserTest.java
@@ -0,0 +1,11 @@
+package fr.xgouchet.axml;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * @author Xavier Gouchet
+ */
+@RunWith(RobolectricTestRunner.class)
+public class AndroidXmlParserTest {
+}
